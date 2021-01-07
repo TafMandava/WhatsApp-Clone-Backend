f8ffcc261adae942ea2fb0cce544e5d12f0343b5
Tafadzwa Mandava <tmandava@mezzanineware.com>
2021-01-07 16:22:04 +0200

    Creating the data schema and assigning a name to the collection

---
+++ dbMessages.js
@@ -0,0 +1,19 @@
+/*
+    This is where we are going to define the data schema
+    Outlines the data structure
+*/
+import mongoose from 'mongoose';
+
+const whatsappSchema = mongoose.Schema({
+    message: String,
+    name: String,
+    timestamp: String
+});
+
+/*
+    Give a name to the collection - messageContent
+    i.e we can have multiple documents. In sql we have one database and many tables
+    In mongo (no sql) collections are similar to tables (in sql)
+    Use the whatsappSchema for this collection
+*/
+export default mongoose.model('messageContent', whatsappSchema);
